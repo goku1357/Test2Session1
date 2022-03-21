85512825875284b24581a511227d6717286923e4
goku1357 <shrishaabysani@gmail.com>
2022-03-21 16:11:22 +0530

    done with p1

--- p1original.c
+++ p1original.c
@@ -1,2 +1,29 @@
 
+#include <stdio.h>
+#include <math.h>
+void input(float *x1, float *y1, float *x2, float *y2)
+{
+    scanf("%f %f %f %f", x1, y1, x2, y2);
+}
+
+void find_distance(float x1, float y1, float x2, float y2, float *area)
+{
+    float q = (x2 - x1) * (x2 - x1);
+    float w = (y2 - y1) * (y2 - y1);
+    float r = q + w;
+    *area = sqrt(r);
+}
+
+void output(float area
+{
+    printf("the distance is %.1f", area);
+}
+
+int main()
+{
+    float x1, y1, x2, y2;
+    input(&x1, &y1, &x2, &y2);
+    float area;
+    find_distance(x1, y1, x2, y2, &area);
+    output(area);
 }
